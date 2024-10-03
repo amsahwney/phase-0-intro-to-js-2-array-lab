ab273288ae0ce6b015feae4766607332cbbf3089
Amrita <amritak.saw@gmail.com>
2024-10-02 20:33:43 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [
+    `Milo`,
+    `Otis`,
+    `Garfield`
+];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const moreCats = [...cats, name]
+    return moreCats
+}
+
+function prependCat(name) {
+    const moreCats = [name, ...cats]
+    return moreCats
+}
+
+function removeLastCat() {
+    const lessCats = cats.slice(0, 2)
+    return lessCats
+}
+
+function removeFirstCat() {
+    const lessCats = cats.slice(1)
+    return lessCats
+}
